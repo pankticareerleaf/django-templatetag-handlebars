3b3d109e1c2ea402eeb10f6631ccf8a0582fe013
pankti459 <pankticareerleaf@gmail.com>
2019-02-19 16:16:13 +0530

    Update templatetag_handlebars.py

--- templatetag_handlebars/templatetags/templatetag_handlebars.py
+++ templatetag_handlebars/templatetags/templatetag_handlebars.py
@@ -34,14 +34,14 @@ def verbatim_tags(parser, token, endtagname):
         if token.contents == endtagname:
             break
 
-        if token.token_type == template.base.TOKEN_VAR:
+        if token.token_type == template.base.TokenType.VAR:
             text_and_nodes.append('{{')
             text_and_nodes.append(token.contents)
 
-        elif token.token_type == template.base.TOKEN_TEXT:
+        elif token.token_type == template.base.TokenType.TEXT:
             text_and_nodes.append(token.contents)
 
-        elif token.token_type == template.base.TOKEN_BLOCK:
+        elif token.token_type == template.base.TokenType.BLOCK:
             try:
                 command = token.contents.split()[0]
             except IndexError:
@@ -58,7 +58,7 @@ def verbatim_tags(parser, token, endtagname):
                     raise
             text_and_nodes.append(node)
 
-        if token.token_type == template.base.TOKEN_VAR:
+        if token.token_type == template.base.TokenType.VAR:
             text_and_nodes.append('}}')
 
     return text_and_nodes
